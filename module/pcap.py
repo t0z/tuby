6013a653f6953a31d4ff6ece26b3877942776454
t0z <sho@ematome.com>
2016-02-12 07:41:36 +0100

    using Toby.log

--- module/pcap.py
+++ module/pcap.py
@@ -1,20 +1,26 @@
-# http://www.binarytides.com/python-packet-sniffer-code-linux/
+try:
+    TUBY = TUBY  # @UndefinedVariable
+except Exception:
+    from tuby.core import TubyStream
+    TUBY = TubyStream()
 
 import socket
 import sys
 import base64
-TUBY = TUBY  # @UndefinedVariable
+
+'''
+    from: http://www.binarytides.com/python-packet-sniffer-code-linux/
+'''
 
 # create an INET, STREAMing socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 except socket.error, msg:
-    print 'Socket could not be created. Error Code : '\
-        + str(msg[0]) + ' Message ' + msg[1]
+    TUBY.log('Exception: Socket could not be created. Error Code : ' +
+             str(msg[0]) + ' Message ' + msg[1])
     sys.exit(1)
 
 # receive a packet
 soread_max = 65535  # 65565
 while True:
     TUBY.stdout.write(base64.b64encode(s.recvfrom(65565)[0].strip()) + '\n')
-    TUBY.stdout.flush()

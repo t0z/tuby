7765e44d2111e327994bd62d9dcadeb8c966fe2e
t0z <sho@ematome.com>
2016-02-14 13:25:00 +0100

    fun with packet

--- module/pcap.py
+++ module/pcap.py
@@ -1,16 +1,17 @@
 try:
     TUBY = TUBY  # @UndefinedVariable
 except Exception:
-    from tuby.core import TubyStream
-    TUBY = TubyStream()
+    from tuby.core import Streams
+    TUBY = Streams()
 
+import platform
 import socket
 import sys
 import base64
+import binascii
+
 '''
     from: http://www.binarytides.com/python-packet-sniffer-code-linux/
-    
-    
 # the public network interface
 HOST = socket.gethostbyname(socket.gethostname())
 
@@ -31,22 +32,57 @@ print s.recvfrom(65565)
 s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
 '''
 
-protos = {
-    'icmp': socket.IPPROTO_ICMP,
-    'icmp6': socket.IPPROTO_ICMPV6,
-    'tcp': socket.IPPROTO_TCP,
-    'udp': socket.IPPROTO_UDP,
-}
+
+def raw_socket():
+    """ create a raw socket"""
+    sock = None
+    system = platform.system().lower()
+    try:
+        if system == 'linux':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                                 socket.IPPROTO_TCP)
+        elif system == 'windows':
+            HOST = socket.gethostbyname(socket.gethostname())
+            # Create a raw socket and bind it to the public interface.
+            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                                 socket.IPPROTO_IP)
+            sock.bind((HOST, 0))
+            # Include IP headers
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+            # Receive all packages.
+            sock.ioctl(socket.SIO_RCVALL,  # @UndefinedVariable
+                       socket.RCVALL_ON)  # @UndefinedVariable
+        else:
+            raise RuntimeError('Unknown platform: %s', platform.system())
+    except socket.error, msg:
+        raise RuntimeError('Socket could not be created.'
+                           '\n\tError Code : %s\n\tMessage ' %
+                           (str(msg[0])), msg[1])
+    # tell kernel not to put in headers, since we are provpkt_iding it
+    return sock
+# protos = {
+#     'icmp': socket.IPPROTO_ICMP,
+#     # 'icmp6': socket.IPPROTO_ICMPV6,
+#     'tcp': socket.IPPROTO_TCP,
+#     'udp': socket.IPPROTO_UDP,
+# }
 # create an INET, STREAMing socket
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, protos['icmp'])
-    # s.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+    sock = raw_socket()
+    # socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_TCP)
 except socket.error, msg:
     TUBY.log('Exception: Socket could not be created. Error Code : ' +
              str(msg[0]) + ' Message ' + msg[1])
     sys.exit(1)
 
 # receive a packet
-soread_max = 65535  # 65565
 while True:
-    TUBY.stdout.write(base64.b64encode(s.recvfrom(65565)[0].strip()) + '\n')
+    try:
+#         TUBY.stdout.write(
+#             base64.b64encode(
+#                 sock.recvfrom(65565)[0].strip()) + '\n')
+        TUBY.stdout.write(base64.b64encode(sock.recvfrom(65565)[0].strip()) + '\n')
+    except KeyboardInterrupt:
+        print "Ctrl-C"
+        break
--- module/pcap_json.py
+++ module/pcap_json.py
@@ -1,23 +1,97 @@
 import socket
 from struct import unpack
 import base64
-from multiprocessing import Pool
-from multiprocessing import log_to_stderr
 import json
 
 """
    from: http://www.binarytides.com/python-packet-sniffer-code-linux/
 """
 
-__pool_worker__ = 3
-
 try:
     TUBY = TUBY  # @UndefinedVariable
 except Exception:
-    from tuby.core import TubyStream
-    TUBY = TubyStream()
+    from tuby.core import Streams
+    TUBY = Streams()
 
-pool = Pool(__pool_worker__)
+
+class Packet(object):
+
+    class Tcp(object):
+
+        def __init__(self, packet):
+            self.packet = packet
+            self._preprocess()
+
+        def _preprocess(self):
+            version_ihl = self.packet.ip.iph[0]
+            # version = version_ihl >> 4
+            ihl = version_ihl & 0xF
+            iph_length = ihl * 4
+            # ttl = iph[5]
+            # protocol = iph[6]
+            # s_addr = socket.inet_ntoa(iph[8])
+            # d_addr = socket.inet_ntoa(iph[9])
+            tcp_header = self.packet.raw[iph_length:iph_length + 20]
+            # now unpack them :)
+            self.tcph = unpack('!HHLLBBHHH', tcp_header)
+
+        def s_port(self):
+            return self.tcph[0]
+
+        def d_port(self):
+            return self.tcph[1]
+
+    class Ip(object):
+
+        def __init__(self, packet):
+            self.packet = packet
+            self._preprocess()
+
+        def _preprocess(self):
+            ip_header = self.packet.raw[0:20]
+            # now unpack them :)
+            self.iph = unpack('!BBHHHBBH4s4s', ip_header)
+
+        def version(self):
+            return self.iph[0] >> 4
+
+        def ttl(self):
+            return self.iph[5]
+
+        def protocol(self):
+            return self.iph[6]
+
+        def s_addr(self):
+            return socket.inet_ntoa(self.iph[8])
+
+        def d_addr(self):
+            return socket.inet_ntoa(self.iph[9])
+
+    def __init__(self, raw):
+        if raw is None or len(raw) < 1:
+            raise RuntimeError('Empty packet data')
+        self.raw = raw
+        self.ip = Packet.Ip(self)
+        self.tcp = Packet.Tcp(self)
+#         self._preprocess()
+#         self.ip = Packet.Ip(self)
+#         self.tcp = Packet.Tcp(self)
+# 
+#     def __getattr__(self, name):
+#         if name in ['ip', 'tcp']:
+#             module = getattr(Packet, ucfirst(name))(self)
+#             print "module: %s" % module
+#             setattr(self, name, module)
+#             if not hasattr(self, name):
+#                 raise RuntimeError('Attribute not set: %s', name)
+
+
+def ucfirst(txt):
+    if txt is None or txt == '':
+        return txt
+    if len(txt) < 2:
+        return txt.upper()
+    return txt[0].upper() + txt[1:].lower()
 
 
 def analyze(packet):
@@ -73,15 +147,14 @@ def analyze(packet):
     TUBY.stdout.write(json.dumps(packet) + '\n')
 
 """Main"""
-log_to_stderr()
-queue = []
-for packet in TUBY.stdin:
-    if packet is None:
-        continue
-    packet = packet.strip()
-    if packet == '':
-        continue
+for line in TUBY.stdin:
+    pkt = None
     try:
-        analyze(base64.b64decode(packet))
+        pkt = Packet(base64.b64decode(line.strip()))
     except Exception as e:
-        print(" Error: %s", e)
+        print('Error: %s' % e)
+        continue
+    TUBY.stdout.write('ip%s %s:%s -> %s:%s\n' %
+                      (pkt.ip.version(),
+                       pkt.ip.s_addr(), pkt.tcp.s_port(),
+                       pkt.ip.d_addr(), pkt.tcp.d_port()))
